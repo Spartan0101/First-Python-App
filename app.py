d4632f61c1d8066a37c7da3c111fcdfbd9e0d3b6
Spartan1010 <tranulovianos@gmail.com>
2024-05-14 11:50:22 +0100

    Start New Project

---
+++ app.py
@@ -0,0 +1,8 @@
+#print("Hello World!")
+name = input("What's your name ?")
+print("Hello " + name)
+age = input("How old are you ?")
+if int(age) < 18:
+    print("You are too young")
+else:
+    print("You 2 can access the site now")
