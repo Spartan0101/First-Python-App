44c2820a1329de857aab523c2998421420662d2a
Spartan1010 <tranulovianos@gmail.com>
2024-05-15 10:53:03 +0100

    Start New Project

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-#print("Hello World!")
-name = input("What's your name ?")
-print("Hello " + name)
-age = input("How old are you ?")
-if int(age) < 18:
-    print("You are too young")
-else:
-    print("You 2 can access the site now")
+#print("Hello \n "+ "Ian")
+#print(len(input("Enter your name: ")))
+#name = input("Enter your name: ")
+#print("Hello : " + name)
+# band name generator
+
+print("Welcome to the band name generator!")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name culd be : " + city + " " + pet)
+
